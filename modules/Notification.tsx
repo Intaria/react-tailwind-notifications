2dae50ba06820efc6400846586ba047f4fb35517
Intaria <35311420+Intaria@users.noreply.github.com>
2021-10-22 22:46:48 +0500

    Update Notification.tsx
    remove unnecessary components export

--- modules/Notification.tsx
+++ modules/Notification.tsx
@@ -1,7 +1,3 @@
 export { useNotifications } from './Notification/hooks/useNotifications';
-
-export { NotificationItem } from './Notification/views/Item';
 export { NotificationContainer } from './Notification/views/Container';
-export { useStore as useNotificationsStore } from './Notification/store';
-
 export { notification } from './Notification/';
